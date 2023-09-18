f5830883652953e05404af5f5c98194a0301a6ca
notJustus <justus.beck@gmx.de>
2023-09-18 17:13:55 +0200

    Basic layout

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(...todos, {id: crypto.randomUUID(), title: newItem, completed: false })
+  }
+  console.log(todos)
+
+  return (
+  <>
+  <form onSubmit={handleSubmit} className="formComp">
+    <div className="form-row">
+      <label htmlFor="item">New item</label>
+      <input 
+      value={newItem} 
+      onChange={e => setNewItem(e.target.value)} 
+      type="text" 
+      id="item"/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item 1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item 2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
+}
